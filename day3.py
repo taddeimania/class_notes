14dd48308493bc0daebb00dc4c3d73910fdb7c0e
taddeimania <jtaddei@gmail.com>
2016-09-14 12:00:13 -0400

    added lists and dictionary file

--- day3.py
+++ day3.py
@@ -1,3 +1,13 @@
 
 for _ in range(5):
     print("i am looping")
+
+turns = 0
+while turns < 5:
+    print("WHILE LOOPING")
+    turns += 1
+
+choice = ""
+while choice != "n":
+    print(choice)
+    choice = input("Do you want to loop again? [Y/n] ")
---
+++ day3_collections.py
@@ -0,0 +1,24 @@
+
+my_list = ["joel", 33, ["the dark", "wasps"]]
+
+print(my_list)
+my_list.pop()
+my_list.append("peanut")
+print(my_list)
+print(my_list[1])
+print(my_list[2])
+print(my_list[2][0])
+
+
+my_dictionary = {
+    "name": "Joel",
+    "age": 33,
+    "fears": ["the dark", "wasps"]
+}
+
+print(my_dictionary)
+print(my_dictionary["name"])
+print(my_dictionary["age"])
+print(my_dictionary["fears"])
+
+print(my_dictionary.keys())
