6322d765f2ccb3bb9901a7e819f3f19aa91f0326
taddeimania <jtaddei@gmail.com>
2016-09-15 12:00:02 -0400

    day 4 week 1

---
+++ day4_advanced_collections.py
@@ -0,0 +1,17 @@
+# tuple
+
+my_tuple = ("joel", "sarah")
+print(my_tuple)
+# my_tuple.append("peanut")
+# print(my_tuple)
+
+my_string = "cellular phone"
+print(my_string + "s")
+# my_string = my_string + "s"
+# my_string[2] = "j"
+
+my_set = {2, 7, 9, "joel"}
+my_people_set = {"joel", "sarah", "peanut"}
+print(help(my_set))
+
+print(my_set.intersection(my_people_set))
---
+++ day4_string_interpolation.py
@@ -0,0 +1,11 @@
+mad_lib = """
+Dear {},
+I am {}ing you to {} you
+to {} your {}.
+"""
+
+print(mad_lib.format("Joel", "write", "inform", "get lost",
+                     "face"))
+
+marker_action = "erase"
+print("Markers are difficult to %s" % marker_action)
