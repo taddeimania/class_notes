0705cc81bf0e44bdfb8d67204a9210fe8fcce4c8
taddeimania <jtaddei@gmail.com>
2016-09-20 09:14:22 -0400

    started functions w2d1

---
+++ week2/day1_functions.py
@@ -0,0 +1,13 @@
+
+def square(number):
+    return_value = number ** 2
+    print("WE GOT THE VALUE!")
+    return return_value
+
+print(square(94))
+
+
+def add(left, right):
+    return left + right
+
+print(add(100023, 120938))
