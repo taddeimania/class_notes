96b747f783d239a32b8ce63166711a0bdcb4726d
taddeimania <jtaddei@gmail.com>
2016-09-21 09:57:08 -0400

    started inventory rpg thing

---
+++ week2/day3_code_cleanup.py
@@ -0,0 +1,17 @@
+# Inventory Tracker RPG
+
+# Player
+# Inventory
+# Items
+
+player = {}
+player_name = input("Welcome Traveler! What is your name? ")
+player["name"] = player_name
+print(player)
+
+while True:
+    choice = input("Would you like to (i)nspect your inventory? or (a)dd to your inventory?\n>")
+    if choice == 'a':
+        print("adding item to inventory")
+    elif choice == 'i':
+        print("looking at inventory")
