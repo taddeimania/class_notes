bcdca69e6f264d2fa4593e26daf2b76c8ab9d0fc
taddeimania <jtaddei@gmail.com>
2016-10-04 12:34:16 -0400

    added django stuff

---
+++ week4/playground/my_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    eye_color = models.CharField(max_length=15)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    year = models.IntegerField()
+    captain = models.ForeignKey(Person)
