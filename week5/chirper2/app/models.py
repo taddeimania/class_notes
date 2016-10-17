f90ca2dc1fb9a104c5500794403a8332ba54a101
taddeimania <jtaddei@gmail.com>
2016-10-17 10:43:52 -0400

    added user creation

--- week5/chirper2/app/models.py
+++ week5/chirper2/app/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Chirp(models.Model):
     body = models.CharField(max_length=140)
     created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('auth.User')
+
+    class Meta:
+        ordering = ("-created", )
--- week5/chirper2/app/views.py
+++ week5/chirper2/app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
 
 from app.models import Chirp
 from app.forms import ChirpForm
@@ -13,7 +17,7 @@ def index_view(request):
             instance.save()
     context = {
         "form": ChirpForm(),
-        "all_chirps": Chirp.objects.all().order_by("-created")
+        "all_chirps": Chirp.objects.all()
     }
     return render(request, "index.html", context)
 
@@ -29,3 +33,30 @@ def about_view(request):
 class ChirpView(ListView):
     template_name = "chirps.html"
     model = Chirp
+
+
+class ChirpDetailView(DetailView):
+    model = Chirp
+
+
+class ChirpCreateView(CreateView):
+    model = Chirp
+    success_url = "/chirps"
+    fields = ('body',)
+
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class ChirpUpdateView(UpdateView):
+    model = Chirp
+    success_url = "/chirps"
+    fields = ('body',)
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = "/chirps" # show reverse_lazy
