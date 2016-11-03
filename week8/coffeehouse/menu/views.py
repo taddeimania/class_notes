37b28e00f86e895af357d6840b4ca60b46170135
taddeimania <jtaddei@gmail.com>
2016-11-03 09:57:11 -0400

    added api end point

--- week8/coffeehouse/menu/views.py
+++ week8/coffeehouse/menu/views.py
@@ -2,6 +2,9 @@ from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 
+from rest_framework.generics import ListAPIView
+
+from menu.serializers import SpecialSerializer
 from menu.models import Special
 
 
@@ -24,3 +27,8 @@ class SpecialUpdateView(UpdateView):
     model = Special
     fields = ('title', 'description', 'picture')
     success_url = reverse_lazy("special_list_view")
+
+
+class SpecialListAPIView(ListAPIView):
+    queryset = Special.objects.all()
+    serializer_class = SpecialSerializer
